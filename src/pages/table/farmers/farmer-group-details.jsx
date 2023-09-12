f6dd3eefc9e6a3d9a902c1663881febc78d4f601
AlifSakib <alifsakib@gmail.com>
2023-09-12 17:55:41 +0600

    enough for today

--- src/pages/table/farmers/farmer-group-details.jsx
+++ src/pages/table/farmers/farmer-group-details.jsx
@@ -26,7 +26,7 @@ const COLUMNS = [
       return (
         <div className="flex flex-col">
           <span className="inline-flex items-center">
-            <span className="w-10 h-10 rounded-full ltr:mr-3 rtl:ml-3 flex-none">
+            {/* <span className="w-10 h-10 rounded-full ltr:mr-3 rtl:ml-3 flex-none">
               <img
                 src={`${import.meta.env.VITE_IMG_URL}${
                   row.cell.row?.original?.image
@@ -34,7 +34,7 @@ const COLUMNS = [
                 alt=""
                 className="object-cover w-10 h-10 rounded-full"
               />
-            </span>
+            </span> */}
             <div>
               <p className="text-sm font-bold text-slate-600 dark:text-slate-300 capitalize">
                 {row.cell.row?.original?.full_name}
@@ -45,6 +45,29 @@ const COLUMNS = [
                 <div className="flex items-center gap-1">
                   <p className="text-[10px] underline text-green-600 font-bold">
                     {row?.cell?.row?.original?.farmer_id}
+                  </p>
+                  <p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-3 h-3"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </p>
+                </div>
+              </Link>
+              <Link
+                to={`/farmer-details/${row?.cell?.row?.original?.onboard_by}`}
+              >
+                <div className="flex items-center gap-1">
+                  <p className="text-[10px] underline text-green-600 font-bold">
+                    {row?.cell?.row?.original?.onboard_by}
                   </p>
                   <p>
                     <svg
--- src/pages/table/micro-entrepreneurs/all-micro-entrepreneurs.jsx
+++ src/pages/table/micro-entrepreneurs/all-micro-entrepreneurs.jsx
@@ -111,7 +111,21 @@ const COLUMNS = [
     Header: "Total Farmers",
     accessor: "total_farmer",
     Cell: (row) => {
-      return <span className="font-bold">{row?.cell?.value}</span>;
+      return (
+        <div className="flex items-center gap-1">
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-4 h-4"
+            >
+              <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+            </svg>
+          </p>
+          <span className="font-bold">{row?.cell?.value}</span>
+        </div>
+      );
     },
   },
 
@@ -121,13 +135,58 @@ const COLUMNS = [
     Cell: (row) => {
       return (
         <div>
-          <p className="font-bold">{row?.cell?.value}</p>
-          <p className="text-[10px] text-green-600 font-bold">
-            Email : {row?.cell?.row?.original?.email}
-          </p>
-          <p className="text-[10px] text-slate-900 font-bold">
-            Present Address : {row?.cell?.row?.original?.present_address}
-          </p>
+          <div className="flex items-center gap-1">
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </p>
+            <p className="font-bold text-xs">{row?.cell?.value}</p>
+          </div>
+          <div className="flex gap-1 items-center">
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+                <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+              </svg>
+            </p>
+            <p className="text-[10px] text-green-600 font-bold">
+              {row?.cell?.row?.original?.email}
+            </p>
+          </div>
+          <div className="flex items-center gap-1">
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </p>
+            <p className="text-[10px] text-slate-900 font-bold">
+              Present Address : {row?.cell?.row?.original?.present_address}
+            </p>
+          </div>
         </div>
       );
     },
@@ -137,9 +196,25 @@ const COLUMNS = [
     accessor: "joining_date",
     Cell: (row) => {
       return (
-        <span className="font-bold">
-          {moment(row?.cell?.value).format("llll")}
-        </span>
+        <div className="flex gap-1 items-center">
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </p>
+          <span className="font-bold">
+            {moment(row?.cell?.value).format("llll")}
+          </span>
+        </div>
       );
     },
   },
