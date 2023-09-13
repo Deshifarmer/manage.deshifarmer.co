9cbcf24e8d5bf195a3de9836a7d7c31ddd448430
AlifSakib <alifsakib@gmail.com>
2023-09-13 10:49:07 +0600

    change on map

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import Card from "@/components/ui/Card";
 import RevenueBarChart from "@/components/partials/widget/chart/revenue-bar-chart";
-import RadialsChart from "@/components/partials/widget/chart/radials";
 import SelectMonth from "@/components/partials/SelectMonth";
 import CompanyTable from "@/components/partials/Table/company-table";
 import RecentActivity from "@/components/partials/widget/recent-activity";
 import MostSales from "../../components/partials/widget/most-sales";
-import RadarChart from "../../components/partials/widget/chart/radar-chart";
 import HomeBredCurbs from "./HomeBredCurbs";
 import GroupChart5 from "../../components/partials/widget/chart/group-chart5";
 
